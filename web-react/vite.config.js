026854ac4b2cc974b2413844c3b445a83f6cac59
sunghwanHomePc <>
2026-08-12 13:01:11 +0900

    캐시 수정

--- web-react/vite.config.js
+++ web-react/vite.config.js
@@ -5,5 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        // 빌드될 때마다 파일명 뒤에 고유 해시값을 추가하여 캐시를 무효화함
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  }
 })
+
 
